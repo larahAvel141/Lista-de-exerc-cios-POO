46dd9227575b8665549094f87307a5ded90121be
larahAvel141 <larahavellar44@gmail.com>
2022-06-07 19:54:45 -0300

    Exercicio 9 feito

---
+++ src/Exercicio9/App.java
@@ -0,0 +1,17 @@
+package Exercicio9;
+
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) {
+        
+    /*Faça um programa que receba uma frase qualquer e mostre-a invertida */
+
+    String aux = JOptionPane.showInputDialog("Insira a frase");    
+    String resp = "";  
+    for(int i= (aux.length() -1); i>=0; i--) { 
+        resp += aux.charAt(i);
+    }     
+    JOptionPane.showMessageDialog(null, resp);
+    }
+}
